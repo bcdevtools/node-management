3e3ea7d644fed8b69202a21ac99c3d6c4ad9f5e4
0xbcdev <0xbcdev@bcdev.tools>
2024-05-23 12:42:51 +0700

    remove debugging code

--- cmd/version.go
+++ cmd/version.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"github.com/bcdevtools/node-management/constants"
-	"github.com/shirou/gopsutil/v3/process"
 	"github.com/spf13/cobra"
 	"runtime"
 	"runtime/debug"
@@ -43,18 +42,6 @@ func GetVersionCmd() *cobra.Command {
 
 			if printLongVersion {
 				fmt.Printf("%-11s %s %s/%s\n", "Go:", runtime.Version(), runtime.GOOS, runtime.GOARCH)
-			}
-
-			processes, err := process.Processes()
-			if err != nil {
-				fmt.Println("Error getting processes:", err)
-			} else {
-				fmt.Println("Processes running:")
-				for _, p := range processes {
-					name, _ := p.Cmdline()
-					pid := p.Pid
-					fmt.Printf("  %s (PID: %d)\n", name, pid)
-				}
 			}
 		},
 	}
